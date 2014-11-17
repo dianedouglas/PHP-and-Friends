822b83a87e823f4238a6d0a1fd29a144ffe73c7b
Diane Douglas <diane.douglas1@gmail.com>
2014-11-17 13:05:59 -0800

    Add how to declare a class, create instance of it, access its properties from outside the object and inside of it using this, declare its methods and run them, use methods to modify internal properties, print info about object structure, how to instantiate a class which takes arguments.

---
+++ oophp2_declare_class.php
@@ -0,0 +1,54 @@
+class names are case sensitive, and function names are not.
+
+<?php
+
+	class User
+	{
+		public $name, $password; //2 public properties to hold a user's name and password.
+		function save_user(){
+			echo "\n Here is where we would do stuff to save the user to a database.";
+		}
+		function print_info(){
+			echo "\nname: ". $this->name;
+			echo "\npass: ". $this->password;			
+		}
+		function change_name($new_name){
+			echo "\n changing name... \n";
+			$this->name = $new_name;
+			$this->print_info();
+		}
+	}
+
+	$object = new User;
+	print_r($object); //print info about a variable in human readable format.
+	$object->name = "diane";
+	$object->password = "password123";
+	$object->save_user();
+	$object->print_info();
+	$object->change_name("Jimi Hendrix");
+?>
+
+the keyword new:
+	like Class.new() in ruby.
+		$object = new Classname;
+	creates an object of the specified class.
+	to create a class that has arguments:
+		$user = new User('diane', 'password123');
+
+calling methods on a class and accessing public properties:
+	use '->' instead of '.' in ruby.
+	methods have () on the end and properties don't.
+	** don't use $ when referring to object properties. but use it when you are defining them.
+
+keyword $this:
+	same as self in ruby. to get or set a property of the current object
+	$this->name = "Courtney Love";
+	$this->name_copy = $this->name;
+
+	again, don't use $ when referring to an object's property even though it's defined that way!
+
+
+
+they can go anywhere, before or after statements using them, but generally good practice is to 
+put class and function definitions towards the end of a file. 
+
