48bc6d0411054cf2b340c99b5ce484e475d23309
Diane Douglas <diane.douglas1@gmail.com>
2014-11-18 12:29:57 -0800

    Add multidimensional associative and numerically indexed array example.

--- php_basics9_arrays.php
+++ php_basics9_arrays.php
@@ -73,42 +73,144 @@ Array keyword:
 
 loop through arrays with foreach:
 
-<?php 
+	<?php 
 
-	$things = array("Thing1", "Thing2", "Thing3");
-	$phrases = array("Shakespeare" => "To be or not to be...", "Fairytales" => "Once upon a time...", "Limericks" => "I once knew a man from nantucket...");
+		$things = array("Thing1", "Thing2", "Thing3");
+		$phrases = array("Shakespeare" => "To be or not to be...", "Fairytales" => "Once upon a time...", "Limericks" => "I once knew a man from nantucket...");
 
-	foreach ($things as $thing) {
-		echo $thing . "\n";
-	}
+		foreach ($things as $thing) {
+			echo $thing . "\n";
+		}
 
-	foreach ($phrases as $key => $value) {
-		echo "$key: $value \n";
-	}
+		foreach ($phrases as $key => $value) {
+			echo "$key: $value \n";
+		}
 
-?>
+	?>
 
-foreach is a method that takes 2 or more arguments separated by the keywords 'as' and '=>'
+	foreach is a method that takes 2 or more arguments separated by the keywords 'as' and '=>'
 
-take each item in the array in the variable to the left of the 'as' keyword, 
-and place it in the variable after the 'as' keyword. 
-Then use it in the loop code between the curly brackets.
+	take each item in the array in the variable to the left of the 'as' keyword, 
+	and place it in the variable after the 'as' keyword. 
+	Then use it in the loop code between the curly brackets.
 
-if the array is associative, you can use the '=>' to define variables to place both the key and value into to be used by the code in the loop.
+	if the array is associative, you can use the '=>' to define variables to place both the key and value into to be used by the code in the loop.
 
-note:
+				note:
 
-probably don't need this, but you can also use the each function and the list function to do the same thing. 
-each() takes an array as an argument and returns an array with a key/value pair from the given array. 
-then it moves a builtin pointer to the next pair to return that the next time it is called.
-use this in a while loop with the list function, which maps an array to given variables.
+				probably don't need this, but you can also use the each function and the list function to do the same thing. 
+				each() takes an array as an argument and returns an array with a key/value pair from the given array. 
+				then it moves a builtin pointer to the next pair to return that the next time it is called.
+				use this in a while loop with the list function, which maps an array to given variables.
 
-list($a, $b) = array("Christmas", "Dec 25");
-echo $a . "\n";
-echo $b . "\n";
+				list($a, $b) = array("Christmas", "Dec 25");
+				echo $a . "\n";
+				echo $b . "\n";
 
-while(list($key, $value) = each($myarray)){
-	echo "$key: $value";
-}
+				while(list($key, $value) = each($myarray)){
+					echo "$key: $value";
+				}
 
+multidimensional arrays:
 
+	set them up the same way as normal arrays. 
+	you can nest associative arrays into numerically indexed arrays and vice versa.
+	then loop through them using nested foreach loops.
+
+	example: discographies of 3 bands.
+
+	<?php 
+		$discographies = array(
+			"Neurosis" => array(
+				"Souls at Zero" => array(
+					"To Crawl Under One's Skin",
+					"Souls At Zero",
+					"Zero",
+					"Flight",
+					"The Web",
+					"Sterile Vision",
+					"A Chronology For Survival",
+					"Stripped",
+					"Takeahnase",
+					"Empty" ),
+				"Eye of Every Storm" => array(
+					"Burn",
+					"No River to Take Me Home",
+					"The Eye of Every Storm",
+					"Left to Wander",
+					"Shelter",
+					"A Season in the Sky",
+					"Bridges",
+					"I Can See You" ),
+				"Enemy of the Sun" => array(
+					"Lost",
+					"Raze the Stray",
+					"Burning Flesh in the Year of Pig",
+					"Cold Ascending",
+					"Lexicon" ,
+					"Enemy of the Sun",
+					"The Time of the Beasts",
+					"Cleanse")
+				),
+
+			"Tragedy" => array(
+				"Vengeance" => ar ray(
+					"Conflicting Ideas",
+					"Call To Arms",
+					"Vengeance",
+					"Recurring Nightmare",
+					"Beginning Of The End",
+					"The Lure",
+					"Night Falls",
+					"The Day After",
+					"War Within Us",
+					"Revengeance",
+					"To The Dogs",
+					"No Words"),
+				"Darker Days Ahead" => array(
+					"No Cemeteries Here",	
+					"Close At Hand",
+					"The Grim Infinite",	
+					"The Feeding Hour",
+					"Wail Of Sirens",	
+					"Darker Days Ahead",
+					"Black Against Night",	
+					"Power Fades",
+					"To Earth Like Dust")
+				), 
+
+			"Jimi Hendrix" => array(
+				"Are You Experienced" => array(
+					"Purple Haze",
+					"Manic Depression",
+					"Hey Joe",
+					"Love or Confusion",
+					"May This Be Love",
+					"I Don't Live Today"), 
+				"Axis: Bold as Love" => array(
+					"EXP",
+					"Up from the Skies",
+					"Spanish Castle Magic",
+					"Wait Until Tomorrow",
+					"Ain't No Telling",
+					"Little Wing",
+					"If 6 Was 9"  ) 
+				)
+			); 
+
+		foreach ($discographies as $band => $albums) {
+			echo "$band\n";
+		}
+		foreach ($discographies as $band => $albums) {
+			echo "$band\n";
+			foreach ($albums as $album => $songs){
+				echo "\t$album\n";
+				foreach($songs as $song){
+					echo "\t\t$song\n";
+				}
+			}
+
+		}
+	?>
+
+	This is 3 levels deep. 
