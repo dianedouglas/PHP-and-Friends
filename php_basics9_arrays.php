b4ed954355066cea699e5904232d612bcf7b9cae
Diane Douglas <diane.douglas1@gmail.com>
2014-11-18 14:56:04 -0800

    More multidimensional arrays. Add list of useful array functions.

--- php_basics9_arrays.php
+++ php_basics9_arrays.php
@@ -154,7 +154,7 @@ multidimensional arrays:
 				),
 
 			"Tragedy" => array(
-				"Vengeance" => ar ray(
+				"Vengeance" => array(
 					"Conflicting Ideas",
 					"Call To Arms",
 					"Vengeance",
@@ -209,8 +209,75 @@ multidimensional arrays:
 					echo "\t\t$song\n";
 				}
 			}
-
 		}
+		//print the first song on enemy of the sun by neurosis.
+		echo "\nThe first song on enemy of the sun by neurosis: " . $discographies["Neurosis"]["Enemy of the Sun"][0];
 	?>
 
-	This is 3 levels deep. 
+	This is 3 levels deep. use square brackets to access any one part of it or 3 nested foreach loops.
+
+array functions:
+
+	is_array($variable);
+		returns true/false 
+
+	count($anarray);
+	count($anarray, 0);
+		returns the number of items in the array.
+	 	to count a multidimensional array, give count an extra argument = 0 to only count the top level, or 1 to count all subarray elements too.
+
+	sort($anarray);
+	sort($anarray, SORT_NUMERIC);
+	sort($anarray, SORT_STRING);
+	rsort($anarray, SORT_STRING);
+		this function acts directly on the array instead of returning a new version of sorted elements. 
+		returns true on success, false on failure.
+		rsort is just like sort but in the reverse order. 
+		string = alphabetical, numeric = ascending numeric order.
+
+	shuffle($anarray);
+		put elements in a random order. 
+		like sort, acts directly upon the array. returns true/false for success/failure.
+
+	explode('delimiter', $astring);
+		splits a string into an array by character(s)
+		<?php 
+			print_r(explode(' ', "Hello world, how are you?"));
+		?>
+
+		Array
+		(
+		    [0] => Hello
+		    [1] => world,
+		    [2] => how
+		    [3] => are
+		    [4] => you?
+		)
+
+	extract
+		extract($_GET);
+		unpacks an associative array into variables for later use
+		useful for unpacking the associative array sent from a get request (there's also one for post) 
+		to avoid naming conflicts, use this flag
+		extract($_GET, EXTR_PREFIX_ALL, 'fromget');
+		then all variables holding values will be prefaced with '$fromget_'
+
+	compact
+		opposite of extract. give it a list of variable name stringsand it makes an array out of them.
+
+		<?php 
+			$name = "Diane";
+			$age = 27;
+			$email = "diane.douglas1@gmail.com";
+			$newarray = compact("name", "age", "email");
+			print_r($newarray);
+		?>
+
+	reset
+		reset($anarray);
+		moves internal pointer of which element to select next to the first element of the array in a foreach loop.
+		returns the valueof the element.
+
+	end
+		same as reset but goes to the last element.
+
