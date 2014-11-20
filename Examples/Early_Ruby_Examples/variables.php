61661ef5d4ff6ba1a28fbeaff47a289612d79691
Diane Douglas <diane.douglas1@gmail.com>
2014-11-19 16:01:06 -0800

    Add basic variables exercise from ruby curriculum.

---
+++ Examples/Early_Ruby_Examples/variables.php
@@ -0,0 +1,13 @@
+<?php 
+
+$age_of_bob = 20;
+$my_age = 28;
+$both_ages = $my_age + $age_of_bob;
+$new_age_of_bob = $age_of_bob + 1;
+
+echo "age of bob: $age_of_bob
+my age: $my_age
+both ages summed: $both_ages
+new age of bob: $new_age_of_bob.";
+
+?>
