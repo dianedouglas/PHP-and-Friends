266eb37c6887303d1b1a107f1354b6d03fc08281
Diane Douglas <diane.douglas1@gmail.com>
2014-11-20 11:23:51 -0800

    Add comment to test code about desired result of looping_multiplication function.

--- Examples/Early_Ruby_Examples/tests/Looper_Test.php
+++ Examples/Early_Ruby_Examples/tests/Looper_Test.php
@@ -26,6 +26,8 @@ class Looper_Test extends PHPUnit_Framework_TestCase
 		$result = $test_looper->looping_multiplication(4, 3);
 
 		//Assert
+		//not quite the same as 4*3 because you must take the starting number and add it to itself x times.
+		//so it's really just $starting_number * ($times_to_add_to_itself + 1)
 		$this->assertEquals(16, $result);
 		
 	}
