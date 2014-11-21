e5c629039dc64ccea77ccb157f24ef74729c23a9
Diane Douglas <diane.douglas1@gmail.com>
2014-11-21 13:22:19 -0800

    Add test for final passing condition - pingpong.

--- Examples/Early_Ruby_Examples/tests/Ping_Pong_Test.php
+++ Examples/Early_Ruby_Examples/tests/Ping_Pong_Test.php
@@ -42,6 +42,19 @@ class Ping_Pong_Test extends PHPUnit_Framework_TestCase
 		//Assert
 		$this->assertEquals(array(0, 1, 2, "ping", 4, "pong"), $result);
 	}
+
+	public function test_ping_pong_both_pingpong()
+	{
+		//Arrange
+		$test_ping_pong = new Ping_Pong;
+		$final_number = 15;
+
+		//Act
+		$result = $test_ping_pong->ping_ponger($final_number);
+
+		//Assert
+		$this->assertEquals(array(0, 1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 14, "ping-pong"), $result);
+	}
 }
 
  ?>
