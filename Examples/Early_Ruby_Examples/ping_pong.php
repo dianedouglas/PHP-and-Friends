c381045a469f0d20d3af599c8b856faff44039b8
Diane Douglas <diane.douglas1@gmail.com>
2014-11-21 13:15:27 -0800

    Add the next two bdd steps for going up to first ping, and then first pong.

--- Examples/Early_Ruby_Examples/ping_pong.php
+++ Examples/Early_Ruby_Examples/ping_pong.php
@@ -4,7 +4,16 @@ class Ping_Pong
 {
 	public function ping_ponger($final_number)
 	{
-		return range(0, $final_number);
+		$full_range = range(0, $final_number);
+		foreach ($full_range as $count) {
+			if($count % 3 == 0 && $count != 0){
+				$full_range[$count] = "ping";
+			}
+			elseif ($count % 5 == 0 && $count != 0) {
+				$full_range[$count] = "pong";
+			}
+		}
+		return $full_range;
 	}
 }
 
--- Examples/Early_Ruby_Examples/tests/Ping_Pong_Test.php
+++ Examples/Early_Ruby_Examples/tests/Ping_Pong_Test.php
@@ -16,6 +16,32 @@ class Ping_Pong_Test extends PHPUnit_Framework_TestCase
 		//Assert
 		$this->assertEquals(array(0, 1, 2), $result);
 	}
+
+	public function test_ping_pong_just_ping()
+	{
+		//Arrange
+		$test_ping_pong = new Ping_Pong;
+		$final_number = 3;
+
+		//Act
+		$result = $test_ping_pong->ping_ponger($final_number);
+
+		//Assert
+		$this->assertEquals(array(0, 1, 2, "ping"), $result);
+	}
+
+	public function test_ping_pong_just_pong()
+	{
+		//Arrange
+		$test_ping_pong = new Ping_Pong;
+		$final_number = 5;
+
+		//Act
+		$result = $test_ping_pong->ping_ponger($final_number);
+
+		//Assert
+		$this->assertEquals(array(0, 1, 2, "ping", 4, "pong"), $result);
+	}
 }
 
  ?>
