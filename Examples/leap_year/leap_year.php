ab52c18dbbf432ba002a20f62d99fb43ba861871
Diane Douglas <diane.douglas1@gmail.com>
2014-11-21 15:36:37 -0800

    Refactor to get rid of extra elseif.

--- Examples/leap_year/leap_year.php
+++ Examples/leap_year/leap_year.php
@@ -4,10 +4,7 @@ class Leap_Year_Runner
 {
 	public function leap_year($year)
 	{
-		if ($year % 100 == 0){
-			return false;
-		}
-		elseif ($year % 4 ){
+		if ($year % 100 == 0 || $year % 4){
 			return false;
 		}
 		else {
