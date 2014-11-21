eb7ab697b4b65204e826d861e0519ed6dac96f11
Diane Douglas <diane.douglas1@gmail.com>
2014-11-21 09:37:11 -0800

    Complete exercises with tests for using branching and boolens in methods.

--- Examples/Early_Ruby_Examples/using_branching_and_booleans_in_methods.php
+++ Examples/Early_Ruby_Examples/using_branching_and_booleans_in_methods.php
@@ -5,17 +5,25 @@ class Brancher
 	
 	public function absolutely_larger($input_int)
 	{
-
+		if($input_int >= 0)
+		{
+			$input_int += 1;
+		}
+		else
+		{
+			$input_int -= 1;
+		}
+		return $input_int;
 	}
 	
 	public function can_drink_alcohol($input_int)
 	{
-
+		return $input_int >= 21;
 	}
 
 	public function has_two_digits($input_int)
 	{
-
+		return strlen((string) $input_int) == 2;
 	}
 }
 
