876801b23fcc11d254042eb69ff665bb9e011cba
Diane Douglas <diane.douglas1@gmail.com>
2014-11-21 13:17:17 -0800

    Refactor so that I'm not checking  != 0 twice.

--- Examples/Early_Ruby_Examples/ping_pong.php
+++ Examples/Early_Ruby_Examples/ping_pong.php
@@ -6,10 +6,13 @@ class Ping_Pong
 	{
 		$full_range = range(0, $final_number);
 		foreach ($full_range as $count) {
-			if($count % 3 == 0 && $count != 0){
+			if($count == 0){
+				continue;
+			}
+			elseif($count % 3 == 0){
 				$full_range[$count] = "ping";
 			}
-			elseif ($count % 5 == 0 && $count != 0) {
+			elseif ($count % 5 == 0) {
 				$full_range[$count] = "pong";
 			}
 		}
