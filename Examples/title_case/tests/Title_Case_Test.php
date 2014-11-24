75f552c2d7177c86ecffb35c2ca6dc17db7bd727
Diane Douglas <diane.douglas1@gmail.com>
2014-11-24 10:20:15 -0800

    Add second passing test - two lowercase words.

--- Examples/title_case/tests/Title_Case_Test.php
+++ Examples/title_case/tests/Title_Case_Test.php
@@ -17,6 +17,19 @@ class TitleCase_Test extends PHPUnit_Framework_TestCase
 		//Assert
 		$this->assertEquals("Word", $result);
 	}
+	
+	function test_make_title_case_twowords()
+	{
+		//Arrange
+		$test_classname = new TitleCase;
+		$input_variable = "word up.";
+
+		//Act
+		$result = $test_classname->make_title_case($input_variable);
+
+		//Assert
+		$this->assertEquals("Word Up.", $result);
+	}
 }
 
  ?>
