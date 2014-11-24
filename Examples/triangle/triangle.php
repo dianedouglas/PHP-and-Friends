7e89ccc6723d531c18a1548e7b93cf5e3f6f48de
Diane Douglas <diane.douglas1@gmail.com>
2014-11-24 09:36:37 -0800

    Make the first test pass.

--- Examples/triangle/triangle.php
+++ Examples/triangle/triangle.php
@@ -4,7 +4,10 @@ class Triangle
 {
 	public function triangle_type($side1, $side2, $side3)
 	{
-		return "Triangle Type";
+		if(($side1 + $side2 < $side3) || ($side2 + $side3 < $side1) || ($side1 + $side3 < $side2))
+		{
+			return "Not a triangle.";
+		}
 	}	
 }
 
