08bd2a9b4f03adbd66e2584ce6cf3452f0af2f58
Diane Douglas <dianedouglas@Dianes-MacBook-Air.local>
2014-11-26 00:48:12 -0800

    Add first section of chapter on PHP basics with two examples to explain basic setup of a php file to look at it in the browser, explain primatives, keywords and operators.

---
+++ curriculum/PHPBasics/hellomath.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello PHP!</title>
+  </head>
+  <body>
+    <h1>My first PHP page</h1>
+    <h3>I'm going to use PHP to do some math in your browser so that I don't have to calculate it myself! </h3>
+    <h2>200 x 245 + 999 = <?php echo 300 * 245 + 999; ?></h2>
+  </body>
+</html>
