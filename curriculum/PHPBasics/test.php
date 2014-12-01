700b57af67bf54de6339ad21d6ff05035ac746b3
Diane Douglas <diane.douglas1@gmail.com>
2014-12-01 00:23:15 -0800

    Start functions part 3: scope.

--- curriculum/PHPBasics/test.php
+++ curriculum/PHPBasics/test.php
@@ -5,11 +5,12 @@
 </head>
 <body>
 	<h1>Test my php!</h1>
-	<?php echo "Hello world!";
-		$our_input_word = "stressed";
-		echo "What's the reverse of $our_input_word?  ";
-		echo strrev($our_input_word);
-		echo str_repeat("I'm a function! ", 3);
+	<?php 
+		// echo "Hello world!";
+		// $our_input_word = "stressed";
+		// echo "What's the reverse of $our_input_word?  ";
+		// echo strrev($our_input_word);
+		// echo str_repeat("I'm a function! ", 3);
 
 		// function encode($input_phrase){
 		// 	$reversed_phrase = strrev($input_phrase); //reverse input phrase.
@@ -17,13 +18,20 @@
 		// 	return $capitalized_phrase; //return it as output.
 		// }
 
-		function encode($input_phrase){
-			return strtoupper(strrev($input_phrase));
-		}
+		// function encode($input_phrase){
+		// 	return strtoupper(strrev($input_phrase));
+		// }
 
-		echo $old_string = "David Bowie";
-		$new_string = encode($old_string);
-		echo $new_string;
+		// echo $old_string = "David Bowie";
+		// $new_string = encode($old_string);
+		// echo $new_string;
+
+	function addFive($a_number){
+		$sum = 5 + $a_number;
+	}
+
+	addFive(2); 
+	echo "The sum of 5 and 2 is: $sum";
 
 
 	?>
