c4486387336c3a9af9b5cdfa2a1847308229f43e
Diane Douglas <diane.douglas1@gmail.com>
2014-11-30 21:34:39 -0800

    Add multiple arguments explanation to functions1 and rename. Add second functions section draft. Add notes to roadmap of points to come back and touch on.

--- curriculum/PHPBasics/test.php
+++ curriculum/PHPBasics/test.php
@@ -5,11 +5,11 @@
 </head>
 <body>
 	<h1>Test my php!</h1>
-	<?php echo "Hello world!"; ?>
-	<?php 
+	<?php echo "Hello world!";
 		$our_input_word = "stressed";
 		echo "What's the reverse of $our_input_word?  ";
 		echo strrev($our_input_word);
+		echo str_repeat("I'm a function! ", 3);
 	?>
 </body>
 </html>
